d57caf47d1c858d99d3a5f84dc96b96ce096f121
chris lott <peanutlott96@gmail.com>
2022-03-25 03:07:15 -0400

    commit tree 761c4a7da9171a5979291dcf95edcff5d07c0d99

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = (array) => array.slice(0,2)
+const returnLastTwoDrivers = (array) => array.slice(-2)
+const selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers]
+function createFareMultiplier(fare){
+    return function(){
+        return fare*5
+    }
+}
+const fareDoubler = fare => fare*2
+const fareTripler = fare => fare*3
+function selectDifferentDrivers(driver,array){
+    return array(driver)
+}
